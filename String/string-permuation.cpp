f07b021d8ea355782fda43289fc18322da37ae2a
Joel V Stanley <101164781+JoelVStan@users.noreply.github.com>
2023-07-25 14:47:13 +0530

    Update string-permuation.cpp

--- String/string-permuation.cpp
+++ String/string-permuation.cpp
@@ -1,4 +1,6 @@
-// QUESTION: Write a function to check whether two given strings are Permutation of each other or not. A Permutation of a string is another string that contains same characters, only the order of characters can be different. For example, “abcd” and “dabc” are Permutation of each other.
+// QUESTION: Write a function to check whether two given strings are Permutation of each other or not. 
+// A Permutation of a string is another string that contains same characters, only the order of characters can be different. 
+// For example, “abcd” and “dabc” are Permutation of each other.
 
 
 // compare length of 2 strings
